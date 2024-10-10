58969e4104bb2b59fe6e6b4c4586a6f830e031fd
Peter Hogan <112176057+petehogan70@users.noreply.github.com>
2024-10-10 14:53:50 -0400

    Cleaned and organized raw data

--- location_data.py
+++ location_data.py
@@ -1,11 +1,1 @@
 import pandas as pd
-
-# Load the data
-stop_times = pd.read_csv('stop_times.txt')
-stops = pd.read_csv('stops.txt')
-
-# Filter stop_times to include only rows with trip_id present in trips
-filtered_stops = stops[stops['stop_id'].isin(stop_times['stop_id'])]
-
-# Write the filtered dataframe back to a CSV file
-filtered_stops.to_csv('stops.txt', index=False)
